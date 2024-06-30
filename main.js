9db3a8c0a5a03215235eafe0eabec28223b2d3e5
shorya <94457442+shoryavardhankhyalia@users.noreply.github.com>
2024-06-30 11:18:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,79 @@
+function about()
+{
+    window.location = "about.html";
+}
+function post()
+{
+    document.getElementById("input1").innerHTML = "";
+    document.getElementById("input2").innerHTML = "";
+    document.getElementById("input3").innerHTML = "";
+    alert ("Your Message Is Successfully Send");
+
+    input1 = document.getElementById("input1").value;
+
+    localStorage.setItem("input1", input1);
+}
+function home()
+{
+    window.location = "index.html";
+}
+function vegetable()
+{
+    window.location = "fruit.html";
+}
+function flower()
+{
+    window.location = "flower.html";
+}
+function comment()
+{
+    window.location = "seed.html";
+}
+function dov1()
+{
+    window.location = "tomato.html";
+}
+function dov2()
+{
+    window.location = "brijal.html";
+}
+function dov3()
+{
+    window.location = "brocli.html";
+}
+function dov4()
+{
+    window.location = "cabbage.html";
+}
+function dov5()
+{
+    window.location = "capsicam.html";
+}
+function dov6()
+{
+    window.location = "cauli.html";
+}
+function dov7()
+{
+    window.location = "chilli.html";
+}
+function dov8()
+{
+    window.location = "coriander.html";
+}
+function dov9()
+{
+    window.location = "mint.html";
+}
+function dov10()
+{
+    window.location = "okra.html";
+}
+function dov11()
+{
+    window.location = "radish.html";
+}
+function dov12()
+{
+    window.location = "spinach.html";
+}
